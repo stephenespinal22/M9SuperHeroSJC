a3cb1ec9b0a16ad7bb92a561997033532c943ab2
Stephen Espinal <Stephenespinal22@gmail.com>
2019-10-08 19:52:31 -0400

    creating dto

---
+++ M9/SuperHero/src/main/java/sg/sjc/superhero/dtos/Location.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.sjc.superhero.dtos;
+
+/**
+ *
+ * @author stephenespinal
+ */
+public class Location {
+    
+//    locationId int primary key auto_increment,
+//`name` varchar(255) not null,
+//`description` varchar(255) not null,
+//address varchar(255) not null,
+//longitude decimal(8,6),
+//latitude decimal(8,6)
+    
+    private int locationId;
+    private String name;
+    private String description;
+    private String address;
+    private double longitude;
+    private double latitude;
+    
+}
