78d5bac11732f9bba76da9050e5b4cb7e650cc48
Stephen Espinal <Stephenespinal22@gmail.com>
2019-10-11 17:12:01 -0400

    adding unit tests

--- M9/SuperHero/src/test/java/sg/sjc/superhero/daos/LocationDaoTest.java
+++ M9/SuperHero/src/test/java/sg/sjc/superhero/daos/LocationDaoTest.java
@@ -5,6 +5,7 @@
  */
 package sg.sjc.superhero.daos;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -43,11 +44,10 @@ public class LocationDaoTest {
 
     @Before
     public void setUp() {
-        
+
         List<Location> locationList = locationDao.readAllLocations();
-        
-        for (Location location: locationList)
-        {
+
+        for (Location location : locationList) {
             locationDao.deleteLocation(location.getLocationId());
         }
 
@@ -71,10 +71,10 @@ public class LocationDaoTest {
         location.setAddress("test");
         location.setLatitude(21.000);
         location.setLongitude(21.000);
-        
+
         //act 
         Location newLocation = locationDao.createLocation(location);
-        
+
         //assert 
         assertTrue(location.equals(newLocation));
     }
@@ -84,7 +84,36 @@ public class LocationDaoTest {
      */
     @Test
     public void testReadAllLocations() {
-        
+        //arrange
+        List<Location> locationList = new ArrayList<Location>();
+
+        Location location = new Location();
+        location.setLocationId(1);
+        location.setName("test");
+        location.setDescription("test");
+        location.setAddress("test");
+        location.setLatitude(21.000);
+        location.setLongitude(21.000);
+
+        Location location2 = new Location();
+        location2.setLocationId(2);
+        location2.setName("test");
+        location2.setDescription("test");
+        location2.setAddress("test");
+        location2.setLatitude(21.000);
+        location2.setLongitude(21.000);
+
+        locationList.add(location);
+        locationList.add(location2);
+
+        //act
+        locationDao.createLocation(location);
+        locationDao.createLocation(location2);
+
+        List<Location> locationListToTest = locationDao.readAllLocations();
+
+        //assert
+        assertEquals(locationListToTest, locationList);
     }
 
     /**
@@ -92,6 +121,21 @@ public class LocationDaoTest {
      */
     @Test
     public void testReadLocationById() {
+        //arrange
+        Location location = new Location();
+        location.setLocationId(1);
+        location.setName("test");
+        location.setDescription("test");
+        location.setAddress("test");
+        location.setLatitude(21.000);
+        location.setLongitude(21.000);
+        locationDao.createLocation(location);
+
+        //act
+        Location locationToTest = locationDao.readLocationById(location.getLocationId());
+
+        //assert
+        assertTrue(locationToTest.equals(location));
     }
 
     /**
@@ -99,6 +143,27 @@ public class LocationDaoTest {
      */
     @Test
     public void testUpdateLocation() {
+        //arrange
+        Location location = new Location();
+        location.setLocationId(1);
+        location.setName("test");
+        location.setDescription("test");
+        location.setAddress("test");
+        location.setLatitude(21.000);
+        location.setLongitude(21.000);
+        locationDao.createLocation(location);
+
+        location = locationDao.readLocationById(location.getLocationId());
+
+        location.setDescription("Did it Work?");
+
+        //act
+        locationDao.updateLocation(location);
+
+        Location locationToTest = locationDao.readLocationById(location.getLocationId());
+
+        //assert
+        assertEquals("Did it Work?", locationToTest.getDescription());
     }
 
     /**
@@ -106,6 +171,26 @@ public class LocationDaoTest {
      */
     @Test
     public void testDeleteLocation() {
+        //arrange
+        Location location = new Location();
+        location.setLocationId(1);
+        location.setName("test");
+        location.setDescription("test");
+        location.setAddress("test");
+        location.setLatitude(21.000);
+        location.setLongitude(21.000);
+        locationDao.createLocation(location);
+
+        List<Location> locationList = locationDao.readAllLocations();
+        int listSize = locationList.size();
+
+        //act
+        locationDao.deleteLocation(location.getLocationId());
+        List<Location> locationListToTest = locationDao.readAllLocations();
+        int listSizeToTest = locationListToTest.size();
+
+        //assert
+        assertTrue( (listSize-1) == listSizeToTest);
     }
 
 }
