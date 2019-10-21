28e6535e91a848ec3223630bbd35a496e41da145
jhoan <jhoan@jhoans-MBP-2.fios-router.home>
2019-10-21 16:00:16 -0400

    New Merge

---
+++ M9/SuperHero /src/main/java/sg/sjc/superhero/SuperHeroApplication.java
@@ -0,0 +1,13 @@
+package sg.sjc.superhero;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroApplication.class, args);
+	}
+}
