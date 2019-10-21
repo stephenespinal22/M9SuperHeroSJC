6b6ac8911d6aa415c110ddb9c9fd48abf06cd50f
Stephen Espinal <Stephenespinal22@gmail.com>
2019-10-21 16:51:01 -0400

    fixing bad push

---
+++ M9/SuperHero/src/main/java/sg/sjc/superhero/controllers/SuperPowersController.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.sjc.superhero.controllers;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sg.sjc.superhero.dtos.SuperPowers;
+import sg.sjc.superhero.services.SuperPowersService;
+
+/**
+ *
+ * @author jhoan
+ */
+    @Controller
+public class SuperPowersController {
+
+    private SuperPowersService service;
+
+    @Autowired
+    public SuperPowersController(SuperPowersService service) {
+        this.service = service;
+    }
+
+    @GetMapping("Powers")
+    public String loadPage(Model model) {
+        List<SuperPowers> superPowersList = service.readAllSuperPowers();
+
+        model.addAttribute("superPowersList", superPowersList);
+
+        return "Powers";
+    }
+
+    @PostMapping("addNewSuperPower")
+    public String addSuperPowers(HttpServletRequest request) {
+
+        SuperPowers newSuperPowers = new SuperPowers();
+
+        newSuperPowers.setName(request.getParameter("powerName"));
+
+        service.createSuperPowers(newSuperPowers);
+
+        //tell spring to redirect user to mapping locations
+        return "redirect:/Powers";
+    }
+
+    @GetMapping("deleteSuperPowers")
+    public String deleteSuperPowers(HttpServletRequest request) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        service.deleteSuperPowers(id);
+
+        return "redirect:/Powers";
+    }
+
+    @PostMapping("editSuperPower")
+    public String editSuperPowers(HttpServletRequest request) {
+
+        SuperPowers superPowersToEdit = new SuperPowers();
+
+        superPowersToEdit.setSpwId(Integer.parseInt(request.getParameter("powId")));
+        superPowersToEdit.setName(request.getParameter("powerName"));
+
+        service.updateSuperPowers(superPowersToEdit);
+
+        return "redirect:/Powers";
+    }
+    
+}
