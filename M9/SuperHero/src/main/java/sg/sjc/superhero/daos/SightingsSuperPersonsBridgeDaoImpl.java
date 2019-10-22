d6b327a3156f8bd467125921c306dc668f468414
Stephen Espinal <Stephenespinal22@gmail.com>
2019-10-21 20:08:02 -0400

    adding full many to many to SuperPerson-Organizations

--- M9/SuperHero/src/main/java/sg/sjc/superhero/daos/SightingsSuperPersonsBridgeDaoImpl.java
+++ M9/SuperHero/src/main/java/sg/sjc/superhero/daos/SightingsSuperPersonsBridgeDaoImpl.java
@@ -5,8 +5,22 @@
  */
 package sg.sjc.superhero.daos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 public class SightingsSuperPersonsBridgeDaoImpl implements SightingsSuperPersonsBridgeDao {
+    
+    private final JdbcTemplate jdbc;
+    
+    private final String insertSightingSuperPersonMerge = "Insert Into SuperPersonOrganization(superId, orgId) values (?,?);";
+    private final String deleteSighting = "Delete From SuperPersonOrganization where superId = ?;";
+    private final String deleteSuperPerson = "Delete From SuperPersonOrganization where orgId = ?;";
+    
+     @Autowired
+    public SightingsSuperPersonsBridgeDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbc = jdbcTemplate;
+    }
 
     @Override
     public void createRelationShip(int sightingId, int superId) {
